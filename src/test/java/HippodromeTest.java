640d87e37d6d5d6bffc961e3e1e9f34272966ca3
Dima <dimm85g@mail.ru>
2023-05-20 15:23:33 +0300

    HorseTest completed

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class HippodromeTest {
+    @Test
+    public void ifNullName(){
+        assertThrows(IllegalArgumentException.class,() -> new Hippodrome(null));
+    }
 }
